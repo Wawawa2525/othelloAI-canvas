85ff4d4b33b1477edec5b5ce942e8b9a19360d8a
Wawawa2525 <m2216034sa@ug.jwu.ac.jp>
2024-12-12 14:13:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,15 +12,14 @@ def _requires_from_file(filename):
     return open(filename).read().splitlines()
 
 
-setup(name="kogi-canvas",
-      version="0.1.4",
+setup(name="othelloAI-canvas",
+      version="0.1.0",
       license='Apache',
-      author='Kimio Kuramitsu',
-      description="Canvas from KOGI",
-      url="https://github.com/kuramitsulab/kogi-canvas",
-      packages=['kogi_canvas'],
+      description="A Python library for Othello AI with AI vs AI functionality",
+      url="https://github.com/Wawawa2525/othelloAI-canvas",
+      packages=['othelloAI_canvas'],
       # package_dir={"": "src"},
-      package_data={'kogi_canvas': ['./*.*']},
+      package_data={'othelloAI_canvas': ['./*.*']},
       install_requires=_requires_from_file('requirements.txt'),
       classifiers=[
           'Development Status :: 4 - Beta',
